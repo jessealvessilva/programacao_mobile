4b601fc3d8deab35395de796d1fe157b86e58f17
Jessé Augusto <jessealvessilva@gmail.com>
2024-01-06 16:39:29 -0300

    Declaraçõ de variáveis

---
+++ Dart/fundamentos/main.dart
@@ -0,0 +1,16 @@
+main() {
+  // int a = 7;
+  var a = 7.1; // Dynamic   //
+  double pi = 3.1415;
+  double raio = 10;
+  double area = pi * raio * raio;
+
+  // print("O Valor da variavel é " + a.toString());
+
+  // print("Valor de pi =  " + pi.toString());
+
+  // print( pi is double );
+  // print(a is int);
+
+  print("O valor da área é = " + area.toString());
+}
