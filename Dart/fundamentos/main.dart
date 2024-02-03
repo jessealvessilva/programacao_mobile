3913b72b8511bcb31f9a3cffca89bf13241c7595
Jessé Augusto <jessealvessilva@gmail.com>
2024-02-03 19:25:37 -0300

    Constantes em Dart

--- Dart/fundamentos/main.dart
+++ Dart/fundamentos/main.dart
@@ -1,23 +1,9 @@
 main() {
-  int a = 7;
-  // var    a    = 7.1; // Dynamic   //
-  double pi = 3.1415;
-  String saudacao = "Bom dia";
-  bool estaChovendo = true;
-  // double raio = 10;
-  // double area = pi * raio * raio;
+  //  A = pi * r * r
 
-  // print("O Valor da variavel é " + a.toString());
-  // print("O Valor da variavel é $a ");
+  const double PI = 3.14;
+  double raio = 10.5;
+  double area = PI * raio * raio;
 
-  // print("O Valor da variavel pi é $pi ");
-
-  print("$saudacao");
-
-  // print("Valor de pi =  " + pi.toString());
-
-  // print( pi is double );
-  // print(a is int);
-
-  // print("O valor da área é = " + area.toString());
+  print(" A area da circunferencia é $area");
 }
