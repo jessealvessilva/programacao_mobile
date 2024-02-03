f5c52aec51d623cc38c42f1630d9095418623f06
Jessé Augusto <jessealvessilva@gmail.com>
2024-02-03 19:13:51 -0300

    Tipos de dados em Dart

--- Dart/fundamentos/main.dart
+++ Dart/fundamentos/main.dart
@@ -1,16 +1,23 @@
 main() {
-  // int a = 7;
-  var a = 7.1; // Dynamic   //
+  int a = 7;
+  // var    a    = 7.1; // Dynamic   //
   double pi = 3.1415;
-  double raio = 10;
-  double area = pi * raio * raio;
+  String saudacao = "Bom dia";
+  bool estaChovendo = true;
+  // double raio = 10;
+  // double area = pi * raio * raio;
 
   // print("O Valor da variavel é " + a.toString());
+  // print("O Valor da variavel é $a ");
+
+  // print("O Valor da variavel pi é $pi ");
+
+  print("$saudacao");
 
   // print("Valor de pi =  " + pi.toString());
 
   // print( pi is double );
   // print(a is int);
 
-  print("O valor da área é = " + area.toString());
+  // print("O valor da área é = " + area.toString());
 }
