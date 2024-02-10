f83d148946e1c743c27e63053b8c86f7f97b3a85
Jessé Augusto <jessealvessilva@gmail.com>
2024-02-10 18:30:38 -0300

    Manipulação de Estruturas de dados

---
+++ Dart/fundamentos/listas.dart
@@ -0,0 +1,12 @@
+main() {
+  List numeros = [2, 8, 7, 9, 15];
+  List alunos = ["Ana", "Maria", "Jose", "Pedro", "Joao", "Cappucino", "Zé"];
+
+  // print(numeros);
+  // print(alunos);
+  // print(alunos is List);   // Checar se alunos é uma lista
+  // print(alunos.elementAt(4)); // Acessa o quarto elemento da lista
+  // print(alunos[2]); // Acessa o segundo elemento da lista
+
+  print(alunos.length);
+}
